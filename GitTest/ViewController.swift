92a5f485f83410691cf749f7abf5c7a6145f5f6e
Ricardo <113894@swcsd.us>
2021-03-19 08:59:58 -0400

    First update

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var helloWorld: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func button(_ sender: UIButton) {
+        helloWorld.text = "My name is Ricardo"
+    }
+    
 
 }
 
